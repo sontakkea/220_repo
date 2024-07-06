ac38e07986a3d7cafad9498a74f65184ea2fd3c5
Asontakke680 <Asontakke680@gmail.com>
2024-07-06 10:17:05 +0530

    adding a new project

---
+++ src/com/jbk/demo1/A.java
@@ -0,0 +1,26 @@
+package com.jbk.demo1;
+
+
+public class A {
+	
+	
+	   public static void main(String[] args) {
+		   
+		  
+		    int sum=0;
+		   for ( int i=1; i <=10; i++) {
+			   
+			   sum=sum+i;  
+		   }
+		   System.out.println("======"+sum);
+					   
+					 
+			   
+			
+		}
+		   
+	
+	}
+
+
+
---
+++ src/com/jbk/demo2/A.java
@@ -0,0 +1,26 @@
+package com.jbk.demo2;
+
+public class A {
+	
+	public static void main(String[] args) {
+		
+		
+		int i=0, sum=0, cnt =0;
+		
+		
+		for(i=1; i<=20; i++){
+		
+		sum= sum+i;
+		cnt++;
+		
+		}	
+			
+		System.out.println("--------"+sum);
+		
+		System.out.println("--------" +cnt);
+		
+		
+		
+	}
+
+}
